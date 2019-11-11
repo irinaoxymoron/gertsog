b0041fc19ed73f547dbff2bb3babd83b315b691a
irinaoxymoron <irinaoxymoron@gmail.com>
2019-11-11 17:52:53 +0300

    finished work on the main page

--- app/js/js-main.js
+++ app/js/js-main.js
@@ -20,12 +20,13 @@ $(document).ready(function(){
   });
 });
 
-$('.multiple-items').slick({
-  slidesToShow: 5,
-  slidesToScroll: 5,
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 1,
+  arrows: true,
   autoplay: true,
-  autoplaySpeed: 4000,
-  arrows : false,
+  autoplaySpeed: 3000,
   responsive: [
     {
       breakpoint: 1024,
@@ -34,71 +35,21 @@ $('.multiple-items').slick({
   ]
 });
 
-$('.multiple-items2').slick({
-  slidesToShow: 5,
-  slidesToScroll: 5,
+$('.multiple-items').slick({
+  infinite: true,
+  dots: false,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 6000,
-  arrows : false,
-  responsive: [
-      {
-        breakpoint: 1165,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4
-        }
-      },
-      {
-        breakpoint: 865,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
-        }
-      },
-      {
-        breakpoint: 625,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+  autoplaySpeed: 3000,
+  speed: 1000,
+  fade: true,
+  cssEase: 'linear'
 });
 
-function openLightBox(){
-  var modal = $('.modal');
-$('.show-modal').on('click', function() {
- modal.fadeIn();
-});
-
-$('.close-modal').on('click', function() {
- modal.fadeOut();
-});
-}
-openLightBox();
-
-function openLightBox2(){
-  var modal2 = $('.modal2');
-$('.show-modal2').on('click', function() {
- modal2.fadeIn();
-});
-
-$('.close-modal2').on('click', function() {
- modal2.fadeOut();
-});
-}
-openLightBox2();
-
-
-jQuery(function(){
-  jQuery(".galleryLetters").lightBox();
+$(function(){
+  jQuery("a.review__link").YouTubePopUp();
 });
 
 $(function () { 
